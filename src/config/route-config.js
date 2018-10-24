6e54ad8248cafcdb9e436a36f7f33125ec93452e
Andy Prickett <andy@andyprickett.com>
2018-10-23 19:21:20 -0700

    fix file names in require statements for deploy (remove .js)

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   init(app){
-    const staticRoutes = require("../routes/static.js");
+    const staticRoutes = require("../routes/static");
     
     app.use(staticRoutes);
   }
